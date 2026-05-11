fc3aec078202aaf3528b74cceabee0fa8ab7e136
Justin Pacis <jpacis08@gmail.com>
2026-05-10 21:40:28 -0700

    didn't know the 1st commit didn't show actual progress, this 2ns commit is to show progress made from 2 days ago

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,41 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dealership {
+    // variables
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+
+    // constructor
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+
+    // getters & setters
+    public String getPhone() {return phone;}
+    public void setPhone(String phone) {this.phone = phone;}
+    public String getAddress() {return address;}
+    public void setAddress(String address) {this.address = address;}
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    // methods
+    public ArrayList<Vehicle> getVehiclesByPrice(double min, double max) {return inventory;}
+    public ArrayList<Vehicle> getVehiclesByMakeModel(String make, String model) {return inventory;}
+    public ArrayList<Vehicle> getVehiclesByYear(int min, int max) {return inventory;}
+    public ArrayList<Vehicle> getVehiclesByColor(String color) {return inventory;}
+    public ArrayList<Vehicle> getVehiclesByMileage(int min, int max) {return inventory;}
+    public ArrayList<Vehicle> getVehiclesByType(String vehicleType) {return inventory;}
+    public ArrayList<Vehicle> getAllVehicles() {return inventory;}
+
+    // inventory management
+    public void addVehicle(Vehicle vehicle) {inventory.add(vehicle);}
+    public void removeVehicle(Vehicle vehicle) {inventory.remove(vehicle);}
 }
